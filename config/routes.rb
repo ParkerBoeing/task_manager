41a92c75380df28e838c586eeffc7c2e22d0fdf0
Parker Boeing <parker.boeing@gmail.com>
2023-07-01 10:48:41 -0600

    Feat: build of task manager app

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/", to: "welcome#index"
+
+  get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create" 
 end
